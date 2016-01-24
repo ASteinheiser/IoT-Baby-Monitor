d8a652e08ec5118ae8cfebd3b1e19d47bc293a3f
ASteinheiser <asteinhe@asu.edu>
2016-01-23 19:53:17 -0700

    fixed shit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var five     = require("johnny-five");
 var Particle = require("particle-io");
 var Parse    = require("parse/node");
 
+Parse.initialize("4vgh4jKPecEkJ21tzJ0DbQVmvpc3o7aSckAKv3JJ", "bpQu2ijlZxriI1r4XrRtLandJ3mXqUV2rDWtHUsK");
+
 var board = new five.Board({
   io: new Particle({
     token: 'a2e6ecc21a92fbeb22c212b3f068b1a482374f13',
@@ -9,20 +11,7 @@ var board = new five.Board({
   })
 });
 
-board.on("connect", function() {
-  Parse.initialize("4vgh4jKPecEkJ21tzJ0DbQVmvpc3o7aSckAKv3JJ", "bpQu2ijlZxriI1r4XrRtLandJ3mXqUV2rDWtHUsK");
-});
-
 board.on("ready", function() {
-  var MotionObject = Parse.Object.extend("MotionObject");
-  var motionObject = new MotionObject();
-
-  var NoiseObject = Parse.Object.extend("NoiseObject");
-  var noiseObject = new NoiseObject();
-
-  var FlameObject = Parse.Object.extend("FlameObject");
-  var flameObject = new FlameObject();
-
   var motion = new five.Sensor.Digital("D0");
   var mic    = new five.Sensor("A0");
   var flame  = new five.Sensor.Digital("D1");
@@ -30,6 +19,8 @@ board.on("ready", function() {
   motion.on("change", function() {
     if (this.value == 0) {
       console.log("motion detected");
+      var MotionObject = Parse.Object.extend("MotionObject");
+      var motionObject = new MotionObject();
       motionObject.save({motion: true});
     }
   });
@@ -37,6 +28,8 @@ board.on("ready", function() {
   mic.on("change", function() {
     if (this.value < 500) {
       console.log("sound detected");
+      var NoiseObject = Parse.Object.extend("NoiseObject");
+      var noiseObject = new NoiseObject();
       noiseObject.save({noise: true});
     }
   });
@@ -44,6 +37,8 @@ board.on("ready", function() {
   flame.on("change", function() {
     if (this.value == 1) {
       console.log("fire detected");
+      var FlameObject = Parse.Object.extend("FlameObject");
+      var flameObject = new FlameObject();
       flameObject.save({flame: true});
     }
   });
